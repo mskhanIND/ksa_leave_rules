f2c51c4d1815e48301f020803e7be13817153323
mskhanIND <mskhan.06@gmail.com>
2026-01-14 15:15:17 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,13 @@
+from setuptools import setup, find_packages
 
+setup(
+    name="ksa_leave_rules",
+    version="0.0.1",
+    description="Saudi Arabia compliant sick leave rules for Frappe HRMS",
+    author="Your Company Name",
+    author_email="admin@yourcompany.com",
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=["frappe"],
+)
